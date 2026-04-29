1771fb728356b56bb00102d135f091c221045f55
Christian <polichristian21@gmail.com>
2026-04-29 15:35:20 +0200

    Task2 test

--- Tasks/Task_2.py
+++ Tasks/Task_2.py
@@ -1,12 +1,48 @@
 import sys
 import os
-import random
-import numpy as np
+import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Task_1')))
+
 
 from Task_1 import sha256
 
 def spmac(k: bytearray, u: bytearray) -> str:
-    k.extend(u)
-    return sha256(k)
+    message_to_hash = bytearray(k)
+    message_to_hash.extend(u)
+
+    return sha256(message_to_hash)
+
+def tests():
+    with open("lab3vectors\lab3task2.json", "r") as f:
+        data = json.load(f)
+
+    count = 0
+
+    for test in data:
+        key = bytearray()
+        msg = bytearray()
+        
+        number = test['number']
+        key_hex = test['key']
+        msg_hex = test['msg']
+        tag_hex = test['tag']
+        
+        print(f"--- TEST: {number} ---")
+        print(f"Key (hex): {key_hex}")
+        print(f"Msg (hex): {msg_hex}")
+        print(f"Expected Tag (hex): {tag_hex}\n")
+
+        key = bytearray.fromhex(key_hex)
+        msg = bytearray.fromhex(msg_hex)
+
+        result = spmac(key, msg)
+
+        if result == tag_hex:
+            print("Test: ", number, " passed\n")
+        else:
+            print("Test failed\n")
+
+
+if __name__ == "__main__":
+    tests()
