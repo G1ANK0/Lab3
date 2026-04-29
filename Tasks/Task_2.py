a64bc12b46efcefe1fb2794bf978b405f85a643a
Christian <polichristian21@gmail.com>
2026-04-29 15:01:19 +0200

    Update Task_2.py

--- Tasks/Task_2.py
+++ Tasks/Task_2.py
@@ -10,8 +10,3 @@ from Task_1 import sha256
 def spmac(k: bytearray, u: bytearray) -> str:
     k.extend(u)
     return sha256(k)
-
-if __name__ == "__main__":
-    key = bytearray(b'0\x01')
-    msg = bytearray(b'0\x21')
-    print(spmac(key, msg))
